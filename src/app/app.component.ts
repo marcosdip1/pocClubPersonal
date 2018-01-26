06bd16ab2afc3012bfdcade89b4af10532b33013
Marcos Dip <MDip@proveedor.personal.com.ar>
2018-01-26 17:05:57 -0300

    Cerrar sesion y catalogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
+import { GlobalProvider } from "../providers/global/global";
+import { App  } from 'ionic-angular';
 
 export interface PageInterface {
   title: string;
@@ -21,7 +23,7 @@ export class MyApp {
   rootPage: any = LoginPage;
   //pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,private globals: GlobalProvider, public app: App) {
     this.initializeApp();
   }
 
@@ -34,39 +36,9 @@ export class MyApp {
     });
   }
 
-  openPage(page: PageInterface) {
-    let params = {};
-
-    // The index is equal to the order of our tabs inside tabs.ts
-    if (page.index) {
-      params = { tabIndex: page.index };
-    }
-
-    // The active child nav is our Tabs Navigation
-    if (this.nav.getActiveChildNav() && page.index != undefined) {
-      this.nav.getActiveChildNav().select(page.index);
-    } else {
-      // Tabs are not active, so reset the root page
-      // In this case: moving to or from SpecialPage
-      this.nav.setRoot(page.pageName, params);
-    }
-  }
-
-  isActive(page: PageInterface) {
-    // Again the Tabs Navigation
-    let childNav = this.nav.getActiveChildNav();
-
-    if (childNav) {
-      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
-        return 'primary';
-      }
-      return;
-    }
-
-    // Fallback needed when there is no active childnav (tabs not active)
-    if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
-      return 'primary';
-    }
-    return;
+  doCerrarSession() {
+    this.globals.level = "";
+    this.globals.levelId = "";
+    this.app.getRootNav().setRoot( LoginPage );
   }
 }
